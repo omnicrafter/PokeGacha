ec868507c64dac5b7bb19b83414bcdb7b7662711
Jeff Chen <jeffc@Soma-3.local>
2023-11-07 20:18:40 -0600

    added user login and logout routes and templates

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import requests
 from flask import Flask, render_template, redirect, request, session, g, flash, abort
 from models import db, connect_db, User, Pokemon
-from forms import UserForm
+from forms import UserForm, UserLoginForm
 
 
 CURRENT_USER_KEY = "current_user"
@@ -80,8 +80,53 @@ def signup():
     return render_template("signup.html", form=form)
 
 
+@app.route('/login', methods=["GET", "POST"])
+def handle_login():
+    """Handle User Login"""
+
+    form = UserLoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username, password)
+
+        if user:
+            do_login(user)
+            flash(f'Welcome back, {user.username}')
+            return redirect(f'/users/{user.id}')
+        else:
+            flash("Incorrect Credentials, Please try again.")
+            return redirect('/login')
+
+    if g.user:
+        flash('You are already signed in')
+        return redirect(f'/users/{g.user.id}')
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def handle_logout():
+    """Handle User Logout"""
+
+    if g.user:
+        do_logout()
+        flash('Log out successful')
+        return redirect('/login')
+
+    return redirect('/login')
+
+
 @app.route('/users/<int:user_id>')
-def user_profile(user_id):
+def display_user_profile(user_id):
     user = User.query.get_or_404(user_id)
 
     return render_template('profile.html', user=user)
+
+
+@app.route('/pokeroll')
+def handle_pokeroll():
+    if not g.user:
+        return redirect('login')
--- forms.py
+++ forms.py
@@ -2,15 +2,28 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length
 
+
 class UserForm(FlaskForm):
     """Form for creating a user"""
-        
-    username = StringField('Username', validators = [DataRequired(), Length(max=16)])
-    password = PasswordField('Password', validators=[Length(min=6)])
+
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(max=16)])
+    password = PasswordField('Password', validators=[
+                             DataRequired(), Length(min=6)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    
+
+
+class UserLoginForm(FlaskForm):
+    """Form for logging in user"""
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(max=16)])
+    password = PasswordField('Password', validators=[
+                             DataRequired(), Length(min=6)])
+
+
 class UserEditForm(FlaskForm):
     """Form for editing a user"""
-    
+
+
 class PokemonEditForm(FlaskForm):
     """Form for editing a Pokemon"""
